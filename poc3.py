d3bb700484a00ac9b6bc3bc194ed38d8078f7ce9
Vinod Kumar Kotha <vk21125@corp.ojas-it.com>
2021-05-28 14:05:02 +0530

    poc3

---
+++ poc3.py
@@ -0,0 +1,523 @@
+from tkinter import *
+def song():
+    root = Tk()
+    root.title("Songs")
+    root.geometry("400x300")
+    Label(root, text="select the song to see lyrics").grid(row=2, column=0, columnspan=2)
+    button = Button(root, text = 'Priyatama priyatama.........',width=25,command=priya)
+    button.grid(row=4, column=0, columnspan=2)
+    button = Button(root, text = 'Manohara Na hrudaya.........',width=25,command=man)
+    button.grid(row=6, column=0, columnspan=2)
+    button = Button(root, text = 'Aaduvaari maatalaku.........',width=25,command=aad)
+    button.grid(row=8, column=0, columnspan=2)
+    button = Button(root, text = 'Holi holiya ranga...........',width=25,command=holi)
+    button.grid(row=10, column=0, columnspan=2)
+    button = Button(root, text = 'ye meraa jahaan ............',width=25,command=ye)
+    button.grid(row=12, column=0, columnspan=2)
+    button = Button(root, text = 'Premante suluvu.............',width=25,command=premante)
+    button.grid(row=14, column=0, columnspan=2)
+    button = Button(root, text = 'cheliya ....................',width=25,command=cheliya)
+    button.grid(row=16, column=0, columnspan=2)
+    button = Button(root, text = 'Naraazu gakurama annayya....',width=25,command=na)
+    button.grid(row=18, column=0, columnspan=2)
+    button = Button(root, text = 'Nuvvu sara...................',width=25,command=nuvvu)
+    button.grid(row=20, column=0, columnspan=2)
+    button = Button(root, text = 'ye chota ....................',width=25,command=ye)
+    button.grid(row=22, column=0, columnspan=2)
+def priya():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""ప్రియతమా.. ప్రియతమా.. పలికినది హృదయమే సరిగమా.. 
+                                చిలిపి నీ తలపులో తెలిసినది వలపులో మధురిమా.. 
+                                చెలి చూపు తాకినా.. ఉలకవా పలకవా.. 
+                                వలవేసి వేచి చూస్తున్నా.. దొరకనే దొరకవా 
+
+                                ఇష్టమైన సఖుడా.. ఇష్టమైన సఖుడా.. 
+                                ఒక్కసారి చూడరా.. పిల్లడా 
+                                చక్కనైన.. చుక్కరా చక్కనైనచుక్కరా.. 
+                                నిన్నుకోరు కుందిరా సుందరా.. 
+
+                                ప్రియతమా.. ప్రియతమా.. పలికినది హృదయమే సరిగమా.. 
+                                చిలిపి నీ తలపులో తెలిసినది వలపులో మధురిమా.. 
+
+                                చరణం:1 
+                                నీ ప్రేమలో ఆరాధనై.. నీ నిండుగా మునిగాకా 
+                                నీ కోసమే.. రాశానుగా నా కళ్లతో ప్రియలేఖ 
+                                చేరునో.. చేరదో తెలియదు ఆ కానుక.. 
+                                ఆశనే వీడకా.. వెనుక పడెను మనసు పడిన మనసే 
+
+                                ఇష్టమైన సఖుడా.. ఇష్టమైన సఖుడా.. 
+                                ఒక్కసారి చూడరా.. పిల్లడా 
+
+                                చరణం 2: 
+                                ఉన్నానిలా.. ఉంటానిలా నీ నీడగా కడదాకా 
+                                కన్నీటిలో కార్తీకపు దీపాన్నిరా నువులేక 
+                                దూరమే భారమై.. కదలదు నా జీవితం 
+                                నీవు నా చేరువై.. నిలిచి మసలు మధుర క్షణములెపుడో.. 
+
+                                ‘ప్రియతమా.. ప్రియతమా.. పలికినది హృదయమే సరిగమా.. 
+                                చిలిపి నీ తలపులో తెలిసినది వలపులో మధురిమా.. 
+                                చెలి చూపు తాకినా.. ఉలకవా పలకవా.. 
+                                వలవేసి వేచి చూస్తున్నా.. దొరకనే దొరకవా 
+
+                                ఇష్టమైన సఖుడా.. ఇష్టమైన సఖుడా.. 
+                                ఒక్కసారి చూడరా.. పిల్లడా 
+                                చక్కనైన.. చుక్కరా చక్కనైన చుక్కరా.. 
+                                నిన్నుకోరు కుందిరా సుందరా.."""
+    my_label = Label(root, text=lyr).pack()
+def man():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""మనోహర నా హృదయమునే ఓ మధువనిగా మలిచినానంట
+
+    రతీవర ఆ తేనెలనే ఓ తుమ్మెదవై తాగిపొమ్మంట
+
+    మనోహర నా హృదయమునే ఓ మధువనిగా మలిచినానంట
+
+    రతీవర ఆ తేనెలనే ఓ తుమ్మెదవై తాగిపొమ్మంట
+
+    నా యవ్వనమే నీ పరమై పులకించే వేళ
+
+    నా యదలో ఒక సుఖమే ఊగెనుగా ఉయ్యాల
+
+
+
+    జడివానై ప్రియా నన్నే చేరుకోమ్మా
+
+    శృతి మించుతోంది దాహం ఒక పాన్పుపై పవళిద్దాం
+
+    కసి కసి పందాలెన్నో ఎన్నో కాసి
+
+    నను జయించుకుంటే నేస్తం నా సర్వస్వం అర్పిస్తా
+
+    ఎన్నటికి మాయదుగా చిగురాకు తొడిగే ఈ బంధం
+
+    ప్రతి ఉదయం నిను చూసి చెలరేగిపోవాలీ దేహం
+
+    మనోహర నా హృదయమునే ఓ మధువనిగా మలిచినానంట
+
+    సుధాకర ఆ తేనెలనే ఓ తుమ్మెదవై తాగిపొమ్మంట ఓ ప్రేమా ప్రేమా…..
+
+
+
+    సందె వేళ స్నానం చేసి నన్ను చేరి
+
+    నా చీర కొంగుతో ఒళ్ళు నువ్వు తుడుస్తావే అదొ కావ్యం
+
+    దొంగమల్లే ప్రియా ప్రియా సడే లేక
+
+    వెనకాలనుండి నన్ను హత్తుకుంటావే అదొ కావ్యం
+
+    నీకోసం మదిలోనే గుడి కట్టినానని తెలియనిదా
+
+    ఓసారి ప్రియమరా ఒడిచేర్చుకోవా నీ చెలిని
+
+    మనోహర నా హృదయమునే ఓ మధువనిగా మలిచినానంట
+
+    రతీవర ఆ తేనెలనే ఓ తుమ్మెదవై తాగిపొమ్మంట
+
+    నా యవ్వనమే నీ పరమై పులకించే వేళ
+
+    నా యదలో ఒక సుఖమే ఊగెనుగా ఉయ్యాల"""
+    my_label = Label(root, text=lyr).pack()
+def aad():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Aaduvaari maatalaku ardhaale verule
+aaduvaari maatalaku ardhaale verule ardhaale verule
+ardhaale verule ardhaale verule ardhaale verule
+
+Aaduvaari maatalaku ardhaale verule
+aaduvaari maatalaku ardhaale verule
+
+Aligi tolagi nilichinacho chelimi jeya rammanile
+aligi tolagi nilichinacho chelimi jeya rammanile
+chorava chesi rammanucho mariyaadaga pommanile 
+chorava chesi rammanucho mariyaadaga pommanile
+
+Aaduvaari maatalaku ardhaale verule
+aaduvaari maatalaku ardhaale verule
+
+Visigi nasigi kasirinacho vishayamasalu ishtamule
+visigi nasigi kasirinacho vishayamasalu ishtamule
+tarachi tarachi oosadigina sarasaminka chaalanile 
+tarachi tarachi oosadigina sarasaminka chaalanile
+
+Aaduvaari maatalaku ardhaale verule
+aaduvaari maatalaku ardhaale verule
+"""
+    my_label = Label(root, text=lyr).pack()
+def holi():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Holi holiya ranga holi chamakeliya holi holi holi holi
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+Holi holila ranga holi holila ranga holi holila ranga holi
+Rama chakkani chilaka meeda gupparu evaru aaku pachani pacha rangu
+
+ 
+
+Chuda chakkani nemalikevaru nerparu cheppu egiri duketi chengu chengu
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+
+Oo paalapitta sakunaa meedananta
+Oo mallemogga manasey korenanta
+Chilipi valapu vagaru pogaru kokilalu
+Kalalu kanna kanne vanne korikalu
+Cheruvulona thamarakupai uge manchu muthyamemannadi
+Chinnadani sonthamaina sampangi mukkupullanouthanannadi
+Hoho hoho andamaina chempa meeda
+Hoho hoho kempuvole siggulolike
+Hoho hoho kempulanni erukochi pattu golusu kattukuntaroo
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Oo ekaveera thirugey ledu lera
+Oo poolathara vagachey roshanaara
+Adugu padithe chalu nela adurunule
+Paduchu vaalu chupu padina chedaranule
+Palle kunaleduru vache yelelo yenki paata padutharule
+Achamaina palle seema paatante gundethone aalakisthale
+Hoho hoho ponne chettu needalona
+Hoho hoho putta thene jonna rotte
+Hoho hoho chenthagoodi okkasari nanjukunte entha meluroo
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+Holi holila ranga holi holila ranga holi holila ranga holi
+Holi holila ranga holi holila ranga holi holila ranga holi
+Rama chakkani chilaka meeda gupparu evaru aaku pachani pacha rangu
+Chuda chakkani nemalikevaru nerparu cheppu egiri duketi chengu chengu
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro"""
+    my_label = Label(root, text=lyr).pack()
+def ye():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""ye meraa jahaan ye meraa ghar mera aashiyaan
+ye meri duniyaa theraa kaam kyaa hai yahaa
+Cheerke andhEre soorajko le aa yahaa
+kaanTonki raahe mile kaliyaa biChaade vahaa
+chal too masti me chal jaayegi manzil kahaa
+kal aane vaalonko do theri kadmonki nishaan
+
+itihaaski shuruvaat aagse huyi aag shakti hai aag zindagi hai
+tum sab me ek chingaari hai jise koyi bhujaa nahi sakta
+jo buraayi paas aayEgi jal jaayegi jo paap kareeb aayegaa jal jaayega
+tum sab shole ho mashaalleh ho jalte raho jalte raho jalte raho
+
+ye meraa jahaan ye meraa ghar mera aashiyaan
+ye meri duniyaa theraa kaam kyaa hai yahaa
+Cheerke andhere soorajko le aa yahaa
+kaantonki raahe mile kaliyaa bichaade vahaa
+chal too masti me chal jaayegi manzil kahaa
+kal aane vaalonko de theri kadmonki nishaan
+
+itihaasko badlaa pahiyaa ne pahiya ghoomtaa hai duniya aage badti hai
+tumhe bhi ek din itihaas badalnaa hai desh ko aage badhaana hai
+tum sab aanevaale kalki saanse ho tum sab aanevaale kalki dhadkan ho
+tum ruk nahi sakte tham nahi sakte chalte raho chalte raho chalte raho
+
+ye meraa jahaan ye meraa ghar mera aashiyaan
+ye meri duniyaa theraa kaam kyaa hai yahaa
+Cheerke andhere soorajko le aa yahaa
+kaantonki raahe mile kaliyaa bichaade vahaa
+chal too masti me chal jaayegi manzil kahaa
+kal aane vaalOnko de theri kadmonki nishaan 
+
+"""
+    my_label = Label(root, text=lyr).pack()
+def premante():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Premante suluvu kaadura adi neevu gelavalevura
+ Premincha sharutulemito anduloni marmamemito
+Premento viluva ayinadi andariki dorakalenidi
+Choosenduku chakkanainadi taakaavo bhaggumantadi
+No no no alaa cheppaku manasunte maargamuntadi
+Sayyante chesi chooputa lokaaniki chaati chepputaa
+
+Premante suluvu kaadura adi neevu gelavalevura
+Premincha sharutulemito anduloni marmamemito
+Premento viluva ayinadi andariki dorakalenidi
+Choosenduku chakkanainadi taakaavo bhaggumantadi
+No no no alaa cheppaku manasunte maargamuntadi
+Sayyante chesi chooputa lokaaniki chaati chepputaa
+
+Jaabilini bommaga chesistaava
+Bhoolokam chutti sigalo turimestavaa
+Mabbullo mallela parupestaava
+Aakasam dinduga marchestaava
+Testaavaa testaavaa testaavaa
+Sooryudne patti teccheda nee nudutana botti petteda
+Chukkalato cheera katteda merupulato kaatukettedaa
+
+Taaj mahale nuvvu kattistaava
+Naa kosam niagra jalapaatam testaava
+Everestu sikharamekkistaava
+Pacificu saagarameedestaava
+Vastaavaa testaavaa testaavaa
+Swargaane srusti cheseda nee premaku kaanukiccheda
+Kailaasam bhuviki dinchedaa naa premanu rujuvu chesedaa"""
+    my_label = Label(root, text=lyr).pack()
+def cheliya():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Cheliya cheliya chiru kopamaa chaalayya chaalayya parihaasamu
+kopaalu taapaalu manakelaa saradaaga kaalaanni gadapaala
+salahaalu kalahaalu manakelaa premante padilanga vundaala
+
+Cheliya cheliya chiru kopamaa chaalayya chaalayya parihaasamu
+
+Remmalo mogga ne pooyanu pommante gaali taakanga poochenule
+ayite gaale gelichindananaa leka poove odindananaa
+raallallo silpamlo lopala daagunna uli taakangaa velisenule
+ayite uliye gelichindananaa leka silpam odindananaa
+ee vivaram telipedi evarantaa vyavahaaram teerchedi yevarantaa
+kallallo kadileti kalalantaa oohallo oogedi nuvvantaa
+
+Cheliya cheliya chiru kopamaa chaalayya chaalayya parihaasamu
+
+Neeli meghaalu chiru gaalini dheekonte mabbu vaanalle maarunule
+deenni godavenanukomananaa leka naijam anukonaa
+mouna raagaalu rondu kallanu dheekonte prema vaagalle pongunule
+deeni pralayam anukomananaa leka pranayam anukonaa
+ee vivaram telipedi evarantaa vyavahaaram teerchedi yevarantaa
+adharaalu cheppeti kadhalantaa hrudayamlo medileti valapantaa
+
+Cheliya cheliya chiru kopamaa chaalayya chaalayya parihaasamu"""
+    my_label = Label(root, text=lyr).pack()
+def na():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Manishi Puttunaka puttinindi Matame
+Putti a manishi ne venaka nettinidi matame
+Thalli kadupoloninundi vellinatti manishi talachuku e chedda gatanu
+
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Manarooju manakundi manyayyaa
+Tanavu kaani chota
+Nu adikudanna maata
+Anavaddu nanta nanna mem annagari maata
+Vinaleda nuvvu peta bangaaru paluku maata
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Manarooju manakundi manyayyaa
+
+Akkannalu Maadannalu Taaneesha mantruluga
+Unnanaade Raamadasu raamuni gudi kattenuga
+Quliqutub shahi prema preyasike chihnamga
+Bhagamati pera bhagyanagaramu nirminchenuga
+Nawabulu nirminchina Nagaramulantuu
+Nawabulu nirminchina Nagaramulantuu
+Kulamataala godavalu manakendukuranna inkendukuranna
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Manarooju manakundi manyayyaa
+
+Vinnava sodaruda monna NIMsu davakhanalo
+Jarigina sanghatanana manavati ke machutonaka
+Tana chaavuki muslimu mana hindu sodarulaluki pranadanaminchhadu tana kidneylanu teesi
+Manashulanta okkatani shashtramanna
+Manashulanta okkatani shashtramanna
+Manshullo saitanlaku pattadanna , idi pattadanna
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Manarooju manakundi manyayyaa
+
+Peerla pandaga ochinda oorlallo manavallu
+Dappulanu egaravesukuntu Kolatalu aadutaru
+Sadaru pandaga ochininda pattamlo prativaru
+dunnapotunadistaru dilkhushilu chestuntaru
+Evademi ante manakemti anta
+Evademi ante manakemti anta
+Jashua vishanarudu nuvveranna
+eppudu nuvveranna
+Naraazu gakurama annayya
+Nazeeru annaya
+Muddula Kannaiah
+Manarooju manakundi manyayyaa"""
+    my_label = Label(root, text=lyr).pack()
+def nuvvu():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Nuvvu sarataguntamanu ranno 
+lekunte sachiurukuntavu ranna
+Nuvvu beeru taagutamano ranno 
+lekunte balchi tannestavu ranna
+Arey brandy whisky rammu ye brandu taagina
+Arey brandy whisky rammu ye brandu taagina
+Jabbu padi nevollu gabbu gabbugavtade
+Ye yaadi intunnava
+Aah!
+Tagginamandu talakekkunante
+Talli chellele ala antavamma
+Mathu madugalo yega paakinaaka
+
+Katti kotha ke pani pedatavamma
+Arey burralona saitanu girruna tirigite
+
+Tirigitee … Uh
+
+burlo saitanu girruna tirigite
+Barra barra naalugu murderlu chestave
+Em Lingam samajytunda
+
+Nuvvu sarataguntamanu ranno 
+lekunte sachiurukuntavu ranna
+
+Ah !
+Mandu gonthulo diggabaddadante
+Ontilo unna nee raktam anta
+Kantilokochi cheru kogaane
+Arey Kanti kindi toolu dibbalaagaubbi
+hoy  Kanti kindi toolu dibbalaagaubbi
+Nanju roogam ochi gunjukoni pothave
+
+Nanju Rogama ? Adentra
+
+Mundu mundu neeke erkytadi po
+
+Nuvvu sarataguntamanu lingam 
+lekunte sachiurukuntavu lingam
+
+agu agara Inka emytundo cheppara
+
+Nee Liver leakytadanna
+Nee heart hyjackytadanna
+Nee kidney kidnapaytadanno
+Nee Bladder blad aytadanna
+Anni poyinaka enni mandulu tinna
+
+Em lingam
+
+Oyyy
+Anni poyinaka eni mandulu tinna
+Akhariki nee bathuku teedingu dinganna
+"""
+    my_label = Label(root, text=lyr).pack()
+def ye():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Ye Chota Nuvunna nee venta vastunna
+Manasu nilavanate elaa aapanu
+Ye gaali istunna neee oose vintunna
+Ee vinta bhavam elaa cheppanu
+Innalu pakkana levu kada ane maata gurtuku raadu kada
+Innaalla ontari tanamantaa ninnu choosi tappuku poyindaa
+
+
+Pedavulakennadu teliyani navvulu
+Parichaym ayinavi neevalanaa
+idi varaku ennadu kalagani aaselu
+Modalavutunnavi neevalanaa
+Emaindo emo nijanga
+Lokam maarindo emo kottagaa
+Ye Chota Nuvunna nee venta vastunna
+Manasu nilavanate elaa aapanu
+Ye gaali istunna neee oose vintunna
+Ee vinta bhavam elaa cheppanu
+
+Ee nadi ratiri naa dari cheraka
+Kaavali undiga nee mamata
+Naa prati oopiri aayuvu poyaga
+Vaadaduga mana premanaka
+Noorellu neeto saagani
+vetike aa swargam manato cherani
+Ye gaali istunna neee oose vintunna
+Ee vinta bhavam elaa cheppanu
+Ye Chota Nuvunna nee venta vastunna
+Manasu nilavanate elaa aapanu"""
+    my_label = Label(root, text=lyr).pack()
+def holi():
+    root = Tk()
+    root.title("priyatama song lyrics")
+    root.geometry("400x300")
+    lyr="""Holi holiya ranga holi chamakeliya holi holi holi holi
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+Holi holila ranga holi holila ranga holi holila ranga holi
+Rama chakkani chilaka meeda gupparu evaru aaku pachani pacha rangu
+
+ 
+
+Chuda chakkani nemalikevaru nerparu cheppu egiri duketi chengu chengu
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+
+Oo paalapitta sakunaa meedananta
+Oo mallemogga manasey korenanta
+Chilipi valapu vagaru pogaru kokilalu
+Kalalu kanna kanne vanne korikalu
+Cheruvulona thamarakupai uge manchu muthyamemannadi
+Chinnadani sonthamaina sampangi mukkupullanouthanannadi
+Hoho hoho andamaina chempa meeda
+Hoho hoho kempuvole siggulolike
+Hoho hoho kempulanni erukochi pattu golusu kattukuntaroo
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Holi holiya ranga holi chamakeliya holi holi holi holi
+Oo ekaveera thirugey ledu lera
+Oo poolathara vagachey roshanaara
+Adugu padithe chalu nela adurunule
+Paduchu vaalu chupu padina chedaranule
+Palle kunaleduru vache yelelo yenki paata padutharule
+Achamaina palle seema paatante gundethone aalakisthale
+Hoho hoho ponne chettu needalona
+Hoho hoho putta thene jonna rotte
+Hoho hoho chenthagoodi okkasari nanjukunte entha meluroo
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro
+Holi holila ranga holi holila ranga holi holila ranga holi
+Holi holila ranga holi holila ranga holi holila ranga holi
+Rama chakkani chilaka meeda gupparu evaru aaku pachani pacha rangu
+Chuda chakkani nemalikevaru nerparu cheppu egiri duketi chengu chengu
+
+Gajje ghallumannadiroo gunde jhallumannadiroo
+Vaana jhallumannadiroo uru ghollumannadiro"""
+    my_label = Label(root, text=lyr).pack()
+
+song()
+
+
+
+
+
+
+
+
+
+
+    
